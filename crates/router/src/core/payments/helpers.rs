5c4f56bbe38a94b56d4574bc1f490be04ebef96e
Manoj Ghorela <118727120+manoj-juspay@users.noreply.github.com>
2023-05-17 23:24:15 +0530

    feat: payout use mca in payout_data (#1195)

--- crates/router/src/core/payments/helpers.rs
+++ crates/router/src/core/payments/helpers.rs
@@ -1713,6 +1713,7 @@ pub async fn insert_merchant_connector_creds_to_config(
     }
 }
 
+#[derive(Clone)]
 pub enum MerchantConnectorAccountType {
     DbVal(domain::MerchantConnectorAccount),
     CacheVal(api_models::admin::MerchantConnectorDetails),
